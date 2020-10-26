553967bb2cc467ca6ad71adfe39067571e1f2074
Michal <michal.lesiowski@gmail.com>
2020-10-26 12:04:13 +0100

    Add files via upload

---
+++ precache-manifest.2e63fa3d9f4cd657c4330b7bd0376afa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ab1095488001dd45208cb885922dab",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d71a83baf1ddd82ca458.bundle.js"
+  },
+  {
+    "url": "runtime~main.d71a83baf1ddd82ca458.bundle.js"
+  },
+  {
+    "revision": "acf3283530329d7b74cb",
+    "url": "static/css/main.c438a943.chunk.css"
+  },
+  {
+    "url": "vendors~main.d71a83baf1ddd82ca458.bundle.js"
+  },
+  {
+    "url": "vendors~main.d71a83baf1ddd82ca458.bundle.js.LICENSE.txt"
+  }
+]);
